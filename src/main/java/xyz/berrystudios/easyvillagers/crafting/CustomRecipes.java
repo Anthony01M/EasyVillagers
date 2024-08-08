81c1229b661fa4f17104da57bef127215fb563d9
Anthony M. <anthony19990maalouf@gmail.com>
2024-08-09 02:38:30 +0300

    fix: check if it should be craftable before adding to recipes

--- src/main/java/xyz/berrystudios/easyvillagers/crafting/CustomRecipes.java
+++ src/main/java/xyz/berrystudios/easyvillagers/crafting/CustomRecipes.java
@@ -4,17 +4,15 @@ import xyz.berrystudios.easyvillagers.EasyVillagers;
 import xyz.berrystudios.easyvillagers.crafting.recipes.FarmerRecipe;
 import xyz.berrystudios.easyvillagers.crafting.recipes.IronFarmRecipe;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CustomRecipes {
-    private final List<CustomRecipe> recipes;
+    private final List<CustomRecipe> recipes = new ArrayList<>();;
 
     public CustomRecipes() {
-        this.recipes = Arrays.asList(
-                new FarmerRecipe(),
-                new IronFarmRecipe()
-        );
+        if (EasyVillagers.getInstance().getCfile().craftable_farmer) recipes.add(new FarmerRecipe());
+        if (EasyVillagers.getInstance().getCfile().craftable_ironfarm) recipes.add(new IronFarmRecipe());
     }
     public void registerAll(EasyVillagers easyVillagers) {
         for (CustomRecipe recipe : recipes) recipe.register(easyVillagers);
